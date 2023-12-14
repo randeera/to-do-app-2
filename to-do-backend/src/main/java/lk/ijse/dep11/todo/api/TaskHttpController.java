87ab5b128bb305fc7d572c634df62722b919e6b7
Kaveendra Randeera <khkrandeera@gmail.com>
2023-12-15 01:05:06 +0530

    add web.xml, schema.sql, and edit configurations

--- to-do-backend/src/main/java/lk/ijse/dep11/todo/api/TaskHttpController.java
+++ to-do-backend/src/main/java/lk/ijse/dep11/todo/api/TaskHttpController.java
@@ -1,11 +1,17 @@
 package lk.ijse.dep11.todo.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
 @CrossOrigin
 public class TaskHttpController {
+
+    @Autowired
+    private DataSource pool;
 }
