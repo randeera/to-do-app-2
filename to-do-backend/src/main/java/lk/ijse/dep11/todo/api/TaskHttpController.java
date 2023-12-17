f23d0b09e9bcf670262936d71c392a77d850baca
Kaveendra Randeera <khkrandeera@gmail.com>
2023-12-17 16:49:48 +0530

    implement deleteTask method

--- to-do-backend/src/main/java/lk/ijse/dep11/todo/api/TaskHttpController.java
+++ to-do-backend/src/main/java/lk/ijse/dep11/todo/api/TaskHttpController.java
@@ -69,6 +69,20 @@ public class TaskHttpController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteTask(@PathVariable("id") int taskId){
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement stmExist = connection
+                    .prepareStatement("SELECT * FROM task WHERE id = ?");
+            stmExist.setInt(1, taskId);
+            if (!stmExist.executeQuery().next()) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task Not Found");
+            }
+
+            PreparedStatement stm = connection.prepareStatement("DELETE FROM task WHERE id=?");
+            stm.setInt(1, taskId);
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
