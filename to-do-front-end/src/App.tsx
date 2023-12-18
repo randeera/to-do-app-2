a35c4c67da87d2c514bc22b402a56f7e72882d65
Kaveendra Randeera <khkrandeera@gmail.com>
2023-12-18 23:48:20 +0530

    implement App.tsx,task-server,Task.tsx,TaskList.txs

--- to-do-front-end/src/App.tsx
+++ to-do-front-end/src/App.tsx
@@ -1,14 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
+import {onAuthStateChanged} from 'firebase/auth';
+import {useUser, useUserDispatcher} from "./context/UseContext.tsx";
+import {auth} from "./firebase.ts";
+import {Loader} from "./component/loader/Loader.tsx";
+import {TaskProvider} from "./context/TaskContext.tsx";
+import {Header} from "./component/header/Header.tsx";
+import {Form} from "./component/form/Form.tsx";
+import {TaskList} from "./component/task-list/TaskList.tsx";
+import {SignIn} from "./component/signin/SignIn.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [loader, setLoader] = useState(true);
+    const user = useUser();
+    const userDispatcher = useUserDispatcher();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setLoader(false);
+            if (user) {
+                userDispatcher({type: 'sign-in', user});
+            } else {
+                userDispatcher({type: 'sign-out'});
+            }
+        });
+        return () => unsubscribe();
+    }, []);
 
   return (
     <>
-        <h1>Hello to-do. now you are ok egrhae</h1>
+        {loader ?   // if (loader)
+            <Loader/>
+            :           // else
+
+            user ?  // if (user)
+                (<>
+                    <Header/>
+                    <TaskProvider>
+                        <Form/>
+                        <TaskList/>
+                    </TaskProvider>
+                </>)
+                :       // else
+                <SignIn/>
+        }
     </>
   )
 }
--- to-do-front-end/src/component/task-list/TaskList.tsx
+++ to-do-front-end/src/component/task-list/TaskList.tsx
@@ -1,6 +1,31 @@
-import "./TaskList.css"
+import './TaskList.css';
+import {Task} from "../task/Task.tsx";
+import {useTaskDispatcher, useTaskList} from "../../context/TaskContext.tsx";
+import {useEffect} from "react";
+import {useUser} from "../../context/UseContext.tsx";
+import {getAllTasks} from "../../service/task-service.tsx";
+
 export function TaskList() {
+    const taskList = useTaskList();
+    const taskDispatcher = useTaskDispatcher();
+    const user = useUser();
+
+    useEffect(() => {
+        getAllTasks(user!.email!).then(taskList => {
+            taskDispatcher({type: 'set-list', taskList});
+        }).catch(err => {
+            alert("Failed to load tasks");
+        })
+        return ()=>{
+            taskDispatcher({type: 'set-list', taskList: []});
+        }
+    }, []);
+
     return (
-        <></>
+        <>
+            {taskList.map(task =>
+                <Task key={task.id} {...task} />
+            )}
+        </>
     );
 }
