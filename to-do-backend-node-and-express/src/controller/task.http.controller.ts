2c6eb38954d8a078af5c134c9954ffcdd05341ba
Kaveendra Randeera <khkrandeera@gmail.com>
2023-12-20 00:24:14 +0530

    Create .env and create service method saveTask

--- to-do-backend-node-and-express/src/controller/task.http.controller.ts
+++ to-do-backend-node-and-express/src/controller/task.http.controller.ts
@@ -1,5 +1,8 @@
 import {Router} from "express";
-import {Request,Response} from "express";
+import  {Request,Response} from "express";
+import mysql from 'mysql2/promise'
+import {TaskTO} from "../to/task.to.js";
+import {ResultSetHeader} from "mysql2";
 
 const controller =Router()
 
@@ -9,10 +12,27 @@ controller.patch('/:id',updateTasks)
 controller.delete('/:id',deleteTasks);
 export {controller as TaskHttpController}
 
+const  pool = mysql.createPool({
+    database:process.env.DB_NAME,
+    port:+process.env.DB_PORT!,
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    connectionLimit:+process.env.DB_CONNECTION_LIMIT!
+});
+
 async function getAllTasks(req:Request,res:Response){
     res.send("<h1>Task Controller: Get</h1>");
 }
 async function saveTask(req:Request,res:Response){
+    const task=<TaskTO> req.body;
+    const connection =await pool.getConnection();
+    const [{insertId}]=await connection.execute<ResultSetHeader>('INSERT INTO task (description, status, email) VALUES (?,false,?)',
+        [task.description,task.email]);
+    task.id=insertId;
+    task.status=false;
+    res.status(201).json(task);
+    pool.releaseConnection(connection);
 
 }
 async function updateTasks(req:Request,res:Response) {
