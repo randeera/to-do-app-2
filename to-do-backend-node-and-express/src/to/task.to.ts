91db3f996c012da84f2397c1ae08311a12094c35
Kaveendra Randeera <khkrandeera@gmail.com>
2023-12-19 23:39:53 +0530

    connect database and create TaskTo

---
+++ to-do-backend-node-and-express/src/to/task.to.ts
@@ -0,0 +1,7 @@
+export class TaskTO{
+    constructor(public id:number,
+                public description:string,
+                public status:boolean,
+                public email:string) {
+    }
+}
